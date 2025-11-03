1c981eabf038a50573cc2b38ad8c95ff9d5a0ec2
github-ci <you@example.com>
2025-11-03 14:21:26 +0800

    update++++++++++++

--- src/LarkCustomBotBundle.php
+++ src/LarkCustomBotBundle.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\DoctrineAsyncInsertBundle\DoctrineAsyncInsertBundle;
 use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class LarkCustomBotBundle extends Bundle implements BundleDependencyInterface
 {
@@ -18,6 +19,7 @@ class LarkCustomBotBundle extends Bundle implements BundleDependencyInterface
             DoctrineTimestampBundle::class => ['all' => true],
             HttpClientBundle::class => ['all' => true],
             DoctrineAsyncInsertBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
